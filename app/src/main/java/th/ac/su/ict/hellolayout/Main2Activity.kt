8f007a047252c7e43ed8237a352faea8b24599f3
Charlot <THASSANAPINYO_T@silpakorn.edu>
2019-12-23 11:30:21 +0700

    init hellolayout

---
+++ app/src/main/java/th/ac/su/ict/hellolayout/Main2Activity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.ict.hellolayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
